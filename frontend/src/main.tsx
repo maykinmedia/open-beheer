7d111d0efe38e6e5edaf13672ee5994c54810c10
SilviaAmAm <silvia@maykinmedia.nl>
2025-04-08 17:00:16 +0200

    :wrench: Updated frontend build

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider, createBrowserRouter } from "react-router";
-import App from "~/App.tsx";
 
+import App from "./App";
 import "./main.css";
 import {
   HomePage,
