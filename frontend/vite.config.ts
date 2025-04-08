26256394e81e7eb5ea2ce53008324cac3861ec8c
SilviaAmAm <silvia@maykinmedia.nl>
2025-04-08 17:00:16 +0200

    :wrench: Updated frontend build

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -5,6 +5,9 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    assetsDir: "static/assets",
+  },
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "src"),
