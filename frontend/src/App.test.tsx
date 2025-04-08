bd79e38cf94fb408d710ed231e98cabdbc2d96af
Julian Roeland <julianroeland@gmail.com>
2025-04-08 10:23:12 +0200

    :art: chore: add alias to allow imports from `./src` with `~`

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,7 +1,6 @@
 import { render } from "@testing-library/react";
 import { describe, it } from "vitest";
-
-import App from "./App";
+import App from "~/App";
 
 describe("App", () => {
   it("renders without crashing", () => {
--- frontend/src/main.test.ts
+++ frontend/src/main.test.ts
@@ -1,7 +1,6 @@
 import * as ReactDOM from "react-dom/client";
 import { Mock, expect, it, vi } from "vitest";
-
-import "./main";
+import "~/main";
 
 vi.mock("react-dom/client", async () => {
   const actual = await vi.importActual<typeof ReactDOM>("react-dom/client");
