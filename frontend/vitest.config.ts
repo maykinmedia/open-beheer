5eb5556ab20dfef74139a2bb2a370bb2d5b3b763
Sven van de Scheur <svenvandescheur@gmail.com>
2025-04-10 13:01:51 +0200

    :green_heart: test: fix test setup

--- frontend/vitest.config.ts
+++ frontend/vitest.config.ts
@@ -1,6 +1,9 @@
 import { defineConfig } from "vitest/config";
 
+import config from "./vite.config";
+
 export default defineConfig({
+  ...config,
   test: {
     globals: true,
     environment: "jsdom",
