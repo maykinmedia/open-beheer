436413748718c4f1f2b3cdff061956ecd301d5a7
SilviaAmAm <silvia@maykinmedia.nl>
2025-05-22 15:52:24 +0200

    :construction: feat: Getting API docs working with MsgSpec

--- backend/src/openbeheer/catalogi/views.py
+++ backend/src/openbeheer/catalogi/views.py
@@ -1,19 +1,33 @@
-
 from drf_spectacular.utils import extend_schema
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
 from msgspec import Struct
+import enum
+
 
 class TestOption(Struct, frozen=True):
     label: str
     value: str
 
-@extend_schema(
-    responses={
-        200: TestOption
-    }
-)
+
+class OBFieldType(enum.StrEnum):
+    boolean = enum.auto()
+    date = enum.auto()
+    daterange = enum.auto()
+    null = enum.auto()
+    number = enum.auto()
+    string = enum.auto()
+    # jsx = enum.auto()
+
+
+class OBField[T](Struct, rename="camel"):
+    name: str
+    type: OBFieldType
+    value: T | None = None
+
+
 class DummyView(APIView):
+    @extend_schema(responses={200: TestOption, 201: OBField})
     def get(self, request, format=None):
         return Response([{"label": "test", "value": "test"}])
