ae9ba3d42893d0b5e416c4d1b68580e24cb9b9dc
SilviaAmAm <silvia@maykinmedia.nl>
2025-05-22 15:52:24 +0200

    :construction: feat: Getting API docs working with MsgSpec

--- backend/src/openbeheer/catalogi/schema.py
+++ backend/src/openbeheer/catalogi/schema.py
@@ -1,23 +1,43 @@
-from typing import Any
+from typing import Any, Literal
 from drf_spectacular.extensions import OpenApiSerializerExtension
-from drf_spectacular.plumbing import ResolvedComponent
 from drf_spectacular.openapi import AutoSchema
 from drf_spectacular.utils import Direction
-
-from rest_framework.views import APIView
 from msgspec import Struct
-from msgspec.json import schema
-
+from msgspec.json import schema_components
+from drf_spectacular.plumbing import ResolvedComponent
 
 
 class MsgSpecExtension(OpenApiSerializerExtension):
     target_class = Struct
     match_subclasses = True
 
-    def map_serializer(self, auto_schema: AutoSchema, direction: Direction) -> dict[str, Any]:
-        msgspec_schema = schema(self.target)
-        # TODO make schema compatible
+    def map_serializer(
+        self, auto_schema: AutoSchema, direction: Direction
+    ) -> dict[str, Any]:
+        (out,), components = schema_components(
+            (self.target,), ref_template="#/components/schemas/{name}"
+        )
+        # This is the schema of the current component. However, its schema may reference other components that
+        # also need to be registered in the schema.
+        component_schema = components.pop(self.target.__name__)
 
-        return msgspec_schema
+        # Extracting and registering sub components
+        for sub_name, sub_schema in components.items():
+            sub_component = ResolvedComponent(
+                name=sub_name,
+                type=ResolvedComponent.SCHEMA,
+                object=sub_name,
+                schema=sub_schema,
+            )
+            auto_schema.registry.register_on_missing(sub_component)
 
+        # This is a JSON schema and not an OpenAPI schema. We might need to convert it.
+        return component_schema
 
+    def get_name(
+        self,
+        auto_schema: AutoSchema,
+        direction: Literal["request"] | Literal["response"],
+    ) -> str | None:
+        # The parent does self.target.__class__.__name__ which gives StructMeta for OBField
+        return self.target.__name__
