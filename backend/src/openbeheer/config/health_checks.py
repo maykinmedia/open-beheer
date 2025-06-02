5eac617efc75f2d2de205bb924d3b471f39e8db8
SilviaAmAm <silvia@maykinmedia.nl>
2025-06-02 17:15:51 +0200

    :sparkles: [#23] feat: Add health checks for APIConfig model

---
+++ backend/src/openbeheer/config/health_checks.py
@@ -0,0 +1,33 @@
+from django.utils.translation import gettext_lazy as _
+
+from openbeheer.config.models import APIConfig
+from openbeheer.health_checks.types import (
+    HealthCheck,
+    HealthCheckError,
+    HealthCheckResult,
+)
+
+
+class APIConfigHealthCheck(HealthCheck):
+    def get_name(self) -> str:
+        return _("API configuration")
+
+    def run(self) -> HealthCheckResult:
+        errors = []
+
+        api_config = APIConfig.get_solo()
+        if not api_config.selectielijst_api_service:
+            errors.append(
+                HealthCheckError(
+                    code="missing_selectielijst_api",
+                    message=_(
+                        "Withing API configuration, the Selectielijst API service is not selected."
+                    ),
+                    severity="error",
+                )
+            )
+
+        result = HealthCheckResult(
+            check=self, errors=errors, success=(len(errors) == 0)
+        )
+        return result
