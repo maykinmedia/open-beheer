4d7d1965f857a506bcf9902023c2b9c77da6b5f5
Chris Wesseling <chris@maykinmedia.nl>
2025-06-05 12:57:52 +0200

    :memo: [#36] docs: add descriptions

--- backend/src/openbeheer/types/_zgw.py
+++ backend/src/openbeheer/types/_zgw.py
@@ -1,20 +1,38 @@
-from msgspec import Struct
+from typing import Annotated
+from msgspec import Meta, Struct
 
 
 class InvalidParam(Struct):
-    name: str
-    code: str
-    reason: str
+    name: Annotated[str, Meta(description="Naam van het veld met ongeldige gegevens")]
+    code: Annotated[str, Meta(description="Systeemcode die het type fout aangeeft")]
+    reason: Annotated[
+        str, Meta(description="Uitleg wat er precies fout is met de gegevens")
+    ]
 
 
 class ZGWError(Struct, rename="camel"):
-    type: str
-    code: str
-    title: str
-    status: int
-    detail: str
-    instance: str
-    invalid_params: None | list[InvalidParam]
+    code: Annotated[str, Meta(description="Systeemcode die het type fout aangeeft")]
+    title: Annotated[str, Meta(description="Generieke titel voor het type fout")]
+    status: Annotated[int, Meta(description="De HTTP status code")]
+    detail: Annotated[
+        str, Meta(description="Extra informatie bij de fout, indien beschikbaar")
+    ]
+    instance: Annotated[
+        str,
+        Meta(
+            description="URI met referentie naar dit specifiek voorkomen van de fout. Deze kan gebruikt worden in combinatie met server logs, bijvoorbeeld."
+        ),
+    ]
+    invalid_params: list[InvalidParam]
+    type: (
+        Annotated[
+            str,
+            Meta(
+                description="URI referentie naar het type fout, bedoeld voor developers"
+            ),
+        ]
+        | None
+    ) = None
 
 
 class ZGWResponse[T](Struct):
