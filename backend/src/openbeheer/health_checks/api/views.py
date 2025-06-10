10138ac9a0168be5448ac589812d756e093f368f
SilviaAmAm <silvia@maykinmedia.nl>
2025-06-10 10:05:12 +0200

    :sparkles: [#50] feat: Add API docs for health check endpoint

--- backend/src/openbeheer/health_checks/api/views.py
+++ backend/src/openbeheer/health_checks/api/views.py
@@ -1,10 +1,22 @@
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from drf_spectacular.utils import extend_schema, extend_schema_view
+from ..types import HealthCheckSerialisedResult
 
 from ..utils import run_health_checks
 
 
+@extend_schema_view(
+    get=extend_schema(
+        tags=["Health Checks"],
+        summary="Get health checks",
+        description="Obtain information about whether the application is configured correctly.",
+        responses={
+            "200": list[HealthCheckSerialisedResult],
+        },
+    )
+)
 class HealthChecksView(APIView):
     def get(self, request: Request, *args, **kwargs) -> Response:
         results = run_health_checks()
