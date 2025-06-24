4cbca01e09eb3acb4f5e70c7232150223a40f9bf
SilviaAmAm <silvia@maykinmedia.nl>
2025-06-24 12:39:33 +0200

    :sparkles: [#51] feat: Add catch for 504

--- backend/src/openbeheer/utils/decorators.py
+++ backend/src/openbeheer/utils/decorators.py
@@ -1,12 +1,16 @@
 from functools import wraps
-from typing import Callable
+from typing import Callable, ParamSpec
 from rest_framework.response import Response
-from requests.exceptions import ConnectionError
+from requests.exceptions import ConnectionError, ReadTimeout
 
 from openbeheer.types._open_beheer import ExternalServiceError
 
+Params = ParamSpec("Params")
 
-def handle_service_errors(func: Callable) -> Callable:
+
+def handle_service_errors(
+    func: Callable[Params, Response],
+) -> Callable[Params, Response]:
     """Try/except calls to external services
 
     Decorator to avoid crashes in case the external services (like Open Zaak or the Selectielijst API)
@@ -20,7 +24,7 @@ def handle_service_errors(func: Callable) -> Callable:
     """
 
     @wraps(func)
-    def wrapper(*args, **kwargs) -> Response:
+    def wrapper(*args: Params.args, **kwargs: Params.kwargs) -> Response:
         try:
             response = func(*args, **kwargs)
         except ConnectionError:
@@ -31,6 +35,14 @@ def handle_service_errors(func: Callable) -> Callable:
                 status=502,
             )
             return Response(data, status=502)
+        except ReadTimeout:
+            data = ExternalServiceError(
+                title="Timeout error",
+                detail="The request to the external service timed out.",
+                code="timeout_error",
+                status=504,
+            )
+            return Response(data, status=504)
 
         return response
 
