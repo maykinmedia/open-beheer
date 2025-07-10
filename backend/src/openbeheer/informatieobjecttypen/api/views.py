f8798843dd53e7e6c60828aa7151f0975f32afe8
SilviaAmAm <silvia@maykinmedia.nl>
2025-07-10 15:12:28 +0200

    :sparkles: [#46] feat: Add retrieve/put/patch endpoints for iot

--- backend/src/openbeheer/informatieobjecttypen/api/views.py
+++ backend/src/openbeheer/informatieobjecttypen/api/views.py
@@ -1,14 +1,17 @@
 from typing import Mapping
-from openbeheer.api.views import ListView
-from openbeheer.informatieobjecttypen.types import (
+from openbeheer.api.views import DetailView, DetailViewWithoutVersions, ListView
+from ..constants import INFORMATIEOBJECTTYPE_FIELDSETS
+from ..types import (
     InformatieObjectTypeSummary,
     InformatieObjectTypenGetParametersQuery,
 )
-from openbeheer.types._open_beheer import ExternalServiceError, OBField, OBOption
-from openbeheer.types._zgw import ZGWError, ZGWResponse
+from openbeheer.types import DetailResponse, ExternalServiceError, OBField, OBOption
+from openbeheer.types import ZGWError, ZGWResponse
+from openbeheer.types import FrontendFieldsets
 from openbeheer.types.ztc import (
     InformatieObjectType,
     InformatieObjectTypeRequest,
+    PatchedInformatieObjectTypeRequest,
     VertrouwelijkheidaanduidingEnum,
 )
 from ape_pie import APIClient
@@ -76,3 +79,58 @@ class InformatieObjectTypeListView(
             params.zaaktype = f"{api_client.base_url}zaaktypen/{params.zaaktype}"
 
         return params
+
+
+@extend_schema_view(
+    get=extend_schema(
+        operation_id="service_informatieobjecttypen_retrieve_one",
+        tags=["Informatieobjecttypen"],
+        summary="Get an informatieobjecttype",
+        description="Retrive an informatieobjecttype from Open Zaak.",
+        responses={
+            "200": DetailResponse[InformatieObjectType],
+            "400": ZGWError,
+        },
+    ),
+    patch=extend_schema(
+        tags=["Informatieobjecttype"],
+        summary="Patch an informatieobjecttype",
+        description=(
+            "Partially update a informatieobjecttype from Open Zaak. This will work only with "
+            "an Open Zaak API token with the `catalogi.geforceerd-schrijven` permission if the informatieobjecttype is"
+            "not a concept. Otherwise will return a 400."
+        ),
+        request=PatchedInformatieObjectTypeRequest,
+        responses={
+            "200": DetailResponse[InformatieObjectType],
+            "400": ZGWError,
+        },
+    ),
+    put=extend_schema(
+        tags=["Informatieobjecttypen"],
+        summary="Put a informatieobjecttype",
+        description=(
+            "Fully update a informatieobjecttype from Open Zaak. This will work only with "
+            "an Open Zaak API token with the `catalogi.geforceerd-schrijven` permission if the informatieobjecttype is"
+            "not a concept. Otherwise will return a 400."
+        ),
+        request=PatchedInformatieObjectTypeRequest,
+        responses={
+            "200": DetailResponse[InformatieObjectType],
+            "400": ZGWError,
+        },
+    ),
+)
+class InformatieObjectTypeDetailView(
+    DetailViewWithoutVersions, DetailView[InformatieObjectType]
+):
+    data_type = InformatieObjectType
+    endpoint_path = "informatieobjecttypen/{uuid}"
+
+    # TODO: not sure if we should expand the zaaktypen, since the IOT will be
+    # shown under a specific zaaktype. But maybe all the other zaaktypen should be
+    # expanded?
+    expansions = {}
+
+    def get_fieldsets(self) -> FrontendFieldsets:
+        return INFORMATIEOBJECTTYPE_FIELDSETS
