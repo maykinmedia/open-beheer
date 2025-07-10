7c0293f7805cbf45922e79bfed48e54916b7ced8
SilviaAmAm <silvia@maykinmedia.nl>
2025-07-10 15:29:05 +0200

    :white_check_mark: [#46] test: Test delete endpoint

--- backend/src/openbeheer/informatieobjecttypen/tests/test_views.py
+++ backend/src/openbeheer/informatieobjecttypen/tests/test_views.py
@@ -8,6 +8,7 @@ from zgw_consumers.test.factories import ServiceFactory
 
 from openbeheer.accounts.tests.factories import UserFactory
 from openbeheer.utils.open_zaak_helper.data_creation import OpenZaakDataCreationHelper
+from django.test import tag
 
 
 class InformatieObjectTypeListViewTests(VCRMixin, APITestCase):
@@ -23,7 +24,7 @@ class InformatieObjectTypeListViewTests(VCRMixin, APITestCase):
         )
         cls.user = UserFactory.create()
         cls.endpoint = reverse(
-            "api:informatieobjecttypen:informatieobjecttypen-list",
+            "api:informatieobjecttypenn:informatieobjecttypen-list",
             kwargs={"slug": "OZ"},
         )
         cls.helper = OpenZaakDataCreationHelper(service_identifier="OZ")
@@ -150,3 +151,188 @@ class InformatieObjectTypeListViewTests(VCRMixin, APITestCase):
         )
 
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+
+@tag("vcr")
+class InformatieObjectTypeDetailViewTest(VCRMixin, APITestCase):
+    @classmethod
+    def setUpTestData(cls) -> None:
+        super().setUpTestData()
+        cls.service = ServiceFactory.create(
+            api_type=APITypes.ztc,
+            api_root="http://localhost:8003/catalogi/api/v1",
+            client_id="test-vcr",
+            secret="test-vcr",
+            slug="OZ",
+        )
+        cls.user = UserFactory.create()
+
+        cls.helper = OpenZaakDataCreationHelper(service_identifier="OZ")
+
+    def test_not_authenticated(self):
+        endpoint = reverse(
+            "api:informatieobjecttypen:informatieobjecttype-detail",
+            kwargs={"slug": "OZ", "uuid": "ec9ebcdb-b652-466d-a651-fdb8ea787487"},
+        )
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(self.cassette.play_count, 0)
+
+    def test_retrieve_informatieobjecttype(self):
+        iot = self.helper.create_informatieobjecttype()
+
+        assert iot.url
+        iot_uuid = furl(iot.url).path.segments[-1]
+        self.client.force_login(self.user)
+
+        endpoint = reverse(
+            "api:informatieobjecttypen:informatieobjecttypen-detail",
+            kwargs={"slug": "OZ", "uuid": iot_uuid},
+        )
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertNotIn("versions", data)
+        self.assertIn("result", data)
+        self.assertIn("fieldsets", data)
+        self.assertIn("fields", data)
+
+        fields = {f["name"] for f in data["fields"]}
+
+        self.assertSetEqual(fields, set(data["result"].keys()))
+
+        iot_data = data["result"]
+
+        with self.subTest("result"):
+            self.assertIn("url", iot_data)
+            self.assertIn("omschrijving", iot_data)
+            self.assertIn("catalogus", iot_data)
+            self.assertIn("vertrouwelijkheidaanduiding", iot_data)
+            self.assertIn("beginGeldigheid", iot_data)
+            self.assertIn("eindeGeldigheid", iot_data)
+            self.assertIn("concept", iot_data)
+            self.assertIn("trefwoord", iot_data)
+            self.assertIn("omschrijvingGeneriek", iot_data)
+            self.assertIn("zaaktypen", iot_data)
+            self.assertIn("beginObject", iot_data)
+            self.assertIn("eindeObject", iot_data)
+
+        with self.subTest("expand"):
+            self.assertNotIn("_expand", iot_data)
+
+        with self.subTest("fields"):
+            self.assertEqual(data["fields"][2]["name"], "vertrouwelijkheidaanduiding")
+            self.assertEqual(len(data["fields"][2]["options"]), 8)
+
+    def test_patch_informatieobjecttype(self):
+        iot = self.helper.create_informatieobjecttype()
+
+        assert iot.url
+        iot_uuid = furl(iot.url).path.segments[-1]
+        self.client.force_login(self.user)
+
+        endpoint = reverse(
+            "api:informatieobjecttypen:informatieobjecttypen-detail",
+            kwargs={"slug": "OZ", "uuid": iot_uuid},
+        )
+
+        # Now modify the iot
+        self.client.force_login(self.user)
+
+        response = self.client.patch(
+            endpoint,
+            data={"omschrijving": "MODIFIED by test test_patch_informatieobjecttype"},
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertNotIn("versions", data)
+        self.assertIn("result", data)
+        self.assertIn("fieldsets", data)
+
+        with self.subTest("Modified field"):
+            self.assertEqual(
+                data["result"]["omschrijving"],
+                "MODIFIED by test test_patch_informatieobjecttype",
+            )
+
+    def test_put_informatieobjecttype(self):
+        iot = self.helper.create_informatieobjecttype()
+
+        assert iot.url
+        iot_uuid = furl(iot.url).path.segments[-1]
+        self.client.force_login(self.user)
+
+        endpoint = reverse(
+            "api:informatieobjecttypen:informatieobjecttypen-detail",
+            kwargs={"slug": "OZ", "uuid": iot_uuid},
+        )
+
+        # Now modify the iot
+        self.client.force_login(self.user)
+
+        response = self.client.put(
+            endpoint,
+            data={
+                "omschrijving": "MODIFIED by test test_put_informatieobjecttype",
+                "catalogus": iot.catalogus,
+                "vertrouwelijkheidaanduiding": "geheim",
+                "beginGeldigheid": "2025-07-10",
+                "eindeGeldigheid": "2030-07-10",
+                "concept": True,
+                "trefwoord": ["MODIFIED"],
+                "informatieobjectcategorie": "MODIFIED",
+            },
+            format="json",
+        )
+
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        data = response.json()
+
+        self.assertNotIn("versions", data)
+        self.assertIn("result", data)
+        self.assertIn("fieldsets", data)
+
+        iot_data = data["result"]
+
+        # Now check the modified values
+        self.assertEqual(
+            iot_data["omschrijving"], "MODIFIED by test test_put_informatieobjecttype"
+        )
+        self.assertTrue(iot_data["concept"])
+        self.assertEqual(iot_data["vertrouwelijkheidaanduiding"], "geheim")
+        self.assertEqual(iot_data["catalogus"], iot_data["catalogus"])
+        self.assertEqual(iot_data["beginGeldigheid"], "2025-07-10")
+        self.assertEqual(iot_data["eindeGeldigheid"], "2030-07-10")
+        self.assertEqual(iot_data["trefwoord"], ["MODIFIED"])
+        self.assertEqual(iot_data["informatieobjectcategorie"], "MODIFIED")
+
+    def test_delete_informatieobjecttype(self):
+        iot = self.helper.create_informatieobjecttype()
+
+        assert iot.url
+        iot_uuid = furl(iot.url).path.segments[-1]
+        self.client.force_login(self.user)
+
+        endpoint = reverse(
+            "api:informatieobjecttypen:informatieobjecttypen-detail",
+            kwargs={"slug": "OZ", "uuid": iot_uuid},
+        )
+
+        # Now modify the iot
+        self.client.force_login(self.user)
+
+        response = self.client.delete(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+        response = self.client.get(endpoint)
+
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
