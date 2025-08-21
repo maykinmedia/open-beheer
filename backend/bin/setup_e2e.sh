cd469431179fc8ad68a4f51fbf1e418e26bd2996
SilviaAmAm <silvia@maykinmedia.nl>
2025-08-21 16:49:51 +0200

    :wrench: [#172] chore: Add script to prepare frontend

---
+++ backend/bin/setup_e2e.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -ex
+
+cwd="${PWD}"
+toplevel=$(git rev-parse --show-toplevel)
+
+cd "${toplevel}/frontend"
+
+if [ "${SKIP_BUILD}" != "yes" ]; then
+  npm ci
+  export MYKN_API_URL=""
+  npm run build
+fi
+
+ln -s ${toplevel}/frontend/dist/index.html ${toplevel}/backend/src/openbeheer/templates/index.html
+ln -s ${toplevel}/frontend/dist/static/assets ${toplevel}/backend/src/openbeheer/static/assets
