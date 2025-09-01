dbd65b0d2a8646722d4a3d4e0cf7ba6ac3429793
Chris Wesseling <chris@maykinmedia.nl>
2025-09-01 20:21:34 +0200

    :bug: [#177] fix: OBField creation
     - add test that was missing from #178
     - fix OBField creation code and pull it into a function
       the order of get_args return value is not guaranteed:
       https://docs.python.org/3/library/typing.html#typing.get_args

---
+++ backend/src/openbeheer/types/test_ob_types.py
@@ -0,0 +1,99 @@
+from datetime import date
+from enum import Enum
+from typing import Annotated
+from unittest import TestCase
+
+from hypothesis import assume, given, strategies as st  # noqa: F401
+from msgspec import Meta, Struct
+from msgspec.json import decode, encode
+
+from . import ztc
+from ._open_beheer import _camelize, ob_fields_of_type, options
+
+ZTC_DATATYPES = [
+    struct
+    for name in dir(ztc)
+    if (struct := getattr(ztc, name))
+    and isinstance(struct, type)
+    and issubclass(struct, Struct)
+]
+
+ZTC_ENUMS = [
+    struct
+    for name in dir(ztc)
+    if (struct := getattr(ztc, name))
+    and isinstance(struct, type)
+    and issubclass(struct, Enum)
+]
+
+
+@st.composite
+def ztc_struct_instances(draw):
+    struct_type = draw(st.sampled_from(ZTC_DATATYPES))
+    return draw(st.from_type(struct_type))
+
+
+class OBFieldsTest(TestCase):
+    def test_simple_field_type_example(self):
+        class MyStruct(Struct):
+            boolean: bool
+            integer: int
+            ieee_754: float
+            string: Annotated[str, Meta(max_length=49)]
+            text: str
+            date: date
+
+        (b, i, f, s, t, d) = ob_fields_of_type(MyStruct)
+        assert b.name == "boolean"
+        assert b.type == "boolean"
+        assert i.name == "integer"
+        assert i.type == "number"
+        assert f.name == "ieee754"  # camelcased
+        assert f.type == "number"
+        assert s.name == "string"
+        assert s.type == "string"
+        assert t.name == "text"
+        assert t.type == "text"
+        assert d.name == "date"
+        assert d.type == "date"
+
+    @given(ztc_struct_instances())
+    def test_all_fields_are_present(self, instance: Struct):
+        def as_seen_by_frontend(struct) -> dict:
+            """Return struct as the dict that is seen by the frontend
+            This also asserts that all generated OBFields are serializable
+            """
+            return decode(encode(struct), type=dict)
+
+        ob_fields = ob_fields_of_type(type(instance))
+
+        ob_fields_as_dict = map(as_seen_by_frontend, ob_fields)
+        field_names = {field["name"] for field in ob_fields_as_dict}
+
+        instance_dict = as_seen_by_frontend(instance)
+        expected_struct_attributes = set(map(_camelize, instance_dict))
+
+        assert field_names == expected_struct_attributes
+
+
+class OBOptionsTest(TestCase):
+    @given(enum=st.sampled_from(ZTC_ENUMS))
+    def test_length(self, enum):
+        "Every enum member should have an OBOption"
+        ob_options = options(enum)
+        assert len(ob_options) == len(enum)
+
+    @given(enum=st.sampled_from(ZTC_ENUMS))
+    def test_values(self, enum: type[Enum]):
+        "Every OBOption.value should be a valid Enum value"
+        ob_options = options(enum)
+
+        for option in ob_options:
+            assert option.value in enum
+
+    def test_annotated_enum(self):
+        enum_annotation = Annotated[
+            ztc.VertrouwelijkheidaanduidingEnum, Meta(description="blabla")
+        ]
+        ob_options = options(enum_annotation)
+        assert len(ob_options) == len(ztc.VertrouwelijkheidaanduidingEnum)
