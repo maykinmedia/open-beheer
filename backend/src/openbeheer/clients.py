fd01fcae1dfd1c44ba5d0e2e45a3d243c7866117
Chris Wesseling <chris@maykinmedia.nl>
2025-09-11 15:42:04 +0200

    :sparkles: feat: Add selectielijst_client
    Add selectielijst_client and fix ztc_client caching; it now also flushes
    the cache on Service.delete.

--- backend/src/openbeheer/clients.py
+++ backend/src/openbeheer/clients.py
@@ -2,7 +2,7 @@ from functools import cache
 from typing import Iterator, NoReturn, Protocol
 
 from django.core.exceptions import ImproperlyConfigured
-from django.db.models.signals import post_save
+from django.db.models.signals import post_delete, post_save
 from django.dispatch import receiver
 from django.utils.translation import gettext_lazy as __
 
@@ -11,6 +11,8 @@ from msgspec.json import decode
 from zgw_consumers.client import build_client
 from zgw_consumers.constants import APITypes
 from zgw_consumers.models import Service
+
+from openbeheer.config.models import APIConfig
 
 
 @cache
@@ -32,10 +34,43 @@ def ztc_client(slug: str = "") -> APIClient | NoReturn:
     return client
 
 
-@receiver(post_save, sender=Service)
+@cache
+def selectielijst_client() -> APIClient:
+    # This is probably strongly tied to the ZTC service, because
+    # for selectielijsten Open Zaak has a "ReferentieLijstConfig" singleton
+    # service configured with an array of acceptable years.
+    #
+    # I assume it rejects URLs outside that configured service,
+    # but it doesn't expose this in an API. So we can't directly make sure we use the
+    # same one.
+    # XXX: We could add a Healthcheck that tries to fetch a zaaktype and check if
+    # selectielijstProces has the same URL prefix.
+
+    # But given these selectielijsten are nationally defined and published, IMHO
+    # these could live some place centrally and served with
+    # Access-Control-Allow-Origin: *
+    # so the UI can just call them directly.
+
+    try:
+        config = APIConfig.get_solo()
+        if config.selectielijst_api_service is None:
+            raise ValueError
+    except (APIConfig.DoesNotExist, ValueError) as e:
+        raise ImproperlyConfigured(__("No Selectielijst service configured")) from e
+
+    return build_client(config.selectielijst_api_service)
+
+
+@receiver([post_delete, post_save], sender=Service)
 def _(sender, instance, **_):
     if instance.api_type == APITypes.ztc:
         ztc_client.cache_clear()
+    selectielijst_client.cache_clear()
+
+
+@receiver([post_delete, post_save], sender=APIConfig)
+def _(sender, instance, **_):
+    selectielijst_client.cache_clear()
 
 
 class ZGWPagedResponseProtocol[T](Protocol):
