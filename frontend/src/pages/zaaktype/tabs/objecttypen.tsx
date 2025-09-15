2c4bb820d2177ada4caaac34d096e9f24274cf15
SilviaAmAm <silvia@maykinmedia.nl>
2025-09-15 16:57:29 +0200

    :sparkles: [#191] feat: Add options to the objecttype field

--- frontend/src/pages/zaaktype/tabs/objecttypen.tsx
+++ frontend/src/pages/zaaktype/tabs/objecttypen.tsx
@@ -7,22 +7,42 @@ export const TABS_CONFIG_OBJECTTYPEN: TabConfig<TargetType> = {
     {
       expandFields: [
         "objecttype",
-        "anderObjecttype",
-        "beginObject",
-        "eindeObject",
+        {
+          name: "anderObjecttype",
+          type: "boolean",
+          // @ts-expect-error - FIXME: Extending TypedField here.
+          initValue: () => false,
+        },
+        {
+          name: "beginGeldigheid",
+          type: "string", // FIXME: if we use date, the value is not turned into a YYYY-MM-DD format
+          // @ts-expect-error - FIXME: Extending TypedField here.
+          initValue: () => {
+            const today = new Date();
+            return today.toISOString().split("T")[0];
+          },
+        },
+        {
+          name: "eindeGeldigheid",
+          type: "string", // FIXME: if we use date, the value is not turned into a YYYY-MM-DD format
+          // @ts-expect-error - FIXME: Extending TypedField here.
+          initValue: () => {
+            const today = new Date();
+            today.setFullYear(today.getFullYear() + 1);
+            return today.toISOString().split("T")[0];
+          },
+        },
+        {
+          name: "relatieOmschrijving",
+          type: "string",
+          // @ts-expect-error - FIXME: Extending TypedField here.
+          initValue: () => {
+            return "Object relation";
+          },
+        },
       ],
       label: "Objecttypen",
       key: "zaakobjecttypen",
-      valueTransform: {
-        zaakobjecttypen: (zaakobjecttypen) => {
-          return (zaakobjecttypen || []).map((item) => {
-            return {
-              ...item,
-              objecttype: item ? `${item._expand.objecttype?.name}` : "-",
-            };
-          });
-        },
-      },
     },
   ],
 };
