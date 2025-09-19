2f9e17d6912eec25d459c6d24e614fcb4f32b4be
SilviaAmAm <silvia@maykinmedia.nl>
2025-09-19 16:37:00 +0200

    :fire: [#204] fix: Remove cache from retrieve_objecttypen

--- backend/src/openbeheer/helpers.py
+++ backend/src/openbeheer/helpers.py
@@ -1,11 +1,8 @@
-from functools import cache
-
 from openbeheer.api.views import fetch_all
 from openbeheer.clients import objecttypen_client
 from openbeheer.types.objecttypen import ObjectType
 
 
-@cache
 def retrieve_objecttypen() -> dict[str, ObjectType]:
     with objecttypen_client() as ot_client:
         objecttypen = fetch_all(ot_client, "objecttypes", {}, ObjectType)
