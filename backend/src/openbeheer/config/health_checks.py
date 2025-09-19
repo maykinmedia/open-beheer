ef46cd5f7fbd1472db7edbdf28cf9128805690e5
SilviaAmAm <silvia@maykinmedia.nl>
2025-09-19 15:06:01 +0200

    :sparkles: [#216] feat: Add objecttypes to health check

--- backend/src/openbeheer/config/health_checks.py
+++ backend/src/openbeheer/config/health_checks.py
@@ -23,5 +23,16 @@ class APIConfigHealthCheck(HealthCheck):
                 )
             )
 
+        if not api_config.objecttypen_api_service:
+            errors.append(
+                HealthCheckError(
+                    code="missing_objecttype_api",
+                    message=_(
+                        "Within API configuration, the Objecttypes API service is not selected."
+                    ),
+                    severity="error",
+                )
+            )
+
         result = HealthCheckResult(check=self, errors=errors)
         return result
