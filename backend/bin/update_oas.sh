0ba08c80ad1e1b6323e78d6d45d6bb0bc19f1f16
SilviaAmAm <silvia@maykinmedia.nl>
2025-09-29 16:22:01 +0200

    :wrench: chore: Add script to update types

---
+++ backend/bin/update_oas.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+PWD=$(pwd)
+
+go_back() {
+    cd "$PWD" || error "Original working directory $PWD doesn't exist anymore"
+}
+trap go_back EXIT INT TERM
+
+BACKEND_ROOT=$(dirname "${0}")/..
+FRONTEND_ROOT=$(dirname "${0}")/../../frontend
+
+cd $BACKEND_ROOT
+
+src/manage.py spectacular \
+    --validate \
+    --fail-on-warn \
+    --lang=en \
+    --file openbeheer-oas.yaml
+
+cd $FRONTEND_ROOT
+npm ci
+npm run update-types
+
+go_back
