b1cca20be19c6b1d9a1435e7b2f41c3ae136d26a
SilviaAmAm <silvia@maykinmedia.nl>
2025-10-16 16:20:33 +0200

    :sparkles: [#303] feat: Add UI to publish IOT

--- frontend/src/pages/informatieobjecttype/informatieobjecttype.action.ts
+++ frontend/src/pages/informatieobjecttype/informatieobjecttype.action.ts
@@ -9,8 +9,10 @@ export type PatchedBackendIOT =
 
 export type InformatieObjectTypeAction =
   | TypedAction<"UPDATE", PatchedBackendIOT>
+  | TypedAction<"UPDATE_AND_PUBLISH", PatchedBackendIOT>
   | TypedAction<"SET_EDIT_MODE_ON">
-  | TypedAction<"SET_EDIT_MODE_OFF">;
+  | TypedAction<"SET_EDIT_MODE_OFF">
+  | TypedAction<"PUBLISH">;
 
 export async function informatieobjecttypeAction({
   request,
@@ -21,6 +23,10 @@ export async function informatieobjecttypeAction({
   switch (action.type) {
     case "UPDATE":
       return await updateInformatieObjectTypeVersion(action, params);
+    case "PUBLISH":
+      return await publishInformatieObjectTypeVersion(params);
+    case "UPDATE_AND_PUBLISH":
+      return await updateAndPublishInformatieObjectTypeVersion(action, params);
     case "SET_EDIT_MODE_ON":
       return await setEditModeOn();
     case "SET_EDIT_MODE_OFF":
@@ -47,6 +53,49 @@ export async function updateInformatieObjectTypeVersion(
     } catch (e) {
       return await (e as Response).json();
     }
+  }
+
+  const url = new URL(window.location.href);
+  url.searchParams.delete("editing");
+  return redirect(url.toString());
+}
+
+export async function updateAndPublishInformatieObjectTypeVersion(
+  action: InformatieObjectTypeAction,
+  params: Params,
+): Promise<Response> {
+  const informatieobjecttype = action.payload as PatchedBackendIOT;
+
+  try {
+    await request(
+      "PATCH",
+      `/service/${params.serviceSlug}/informatieobjecttypen/${params.informatieobjecttypeUUID}/`,
+      {},
+      informatieobjecttype,
+    );
+    await request(
+      "POST",
+      `/service/${params.serviceSlug}/informatieobjecttypen/${params.informatieobjecttypeUUID}/publish/`,
+      {},
+    );
+  } catch (e) {
+    return await (e as Response).json();
+  }
+
+  const url = new URL(window.location.href);
+  url.searchParams.delete("editing");
+  return redirect(url.toString());
+}
+
+export async function publishInformatieObjectTypeVersion(params: Params) {
+  try {
+    await request(
+      "POST",
+      `/service/${params.serviceSlug}/informatieobjecttypen/${params.informatieobjecttypeUUID}/publish/`,
+      {},
+    );
+  } catch (e) {
+    return await (e as Response).json();
   }
 
   const url = new URL(window.location.href);
