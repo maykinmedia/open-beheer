ef2c8002a5c09e535ad7e8d8991269e6f9a2f189
SilviaAmAm <silvia@maykinmedia.nl>
2025-10-16 16:21:14 +0200

    :white_check_mark: [#303] test: Test publishing IOT e2e

--- backend/src/openbeheer/informatieobjecttypen/tests/e2e/test_detail_page.py
+++ backend/src/openbeheer/informatieobjecttypen/tests/e2e/test_detail_page.py
@@ -152,3 +152,94 @@ def test_edit_and_cancel_informatieobjecttype(page: Page, runner: GherkinRunner)
 
     runner.then.page_should_not_contain_text(page, "TRALALA!")
     runner.then.page_should_contain_text(page, iot.omschrijving)
+
+
+@pytest.mark.e2e
+def test_publish_informatieobjecttype(page: Page, runner: GherkinRunner):
+    ServiceFactory.create(
+        api_type=APITypes.ztc,
+        api_root="http://localhost:8003/catalogi/api/v1",
+        client_id="test-vcr",
+        secret="test-vcr",
+        slug="OZ",
+    )
+    helper = OpenZaakDataCreationHelper(ztc_service_slug="OZ")
+    catalogus = helper.create_catalogus()
+    assert catalogus.url
+    iot = helper.create_informatieobjecttype(catalogus=catalogus.url)
+    assert iot.url
+
+    UserFactory.create(username="johndoe", password="secret")
+
+    runner.when.go_to_root_page(page)
+    runner.when.user_logs_in(page, username="johndoe", password="secret")
+
+    runner.then.path_should_be(page, "/OZ/")
+
+    runner.when.select_catalogus(page, catalogus)
+    runner.when.go_to_informatieobjecttype_list_page(page, catalogus)
+
+    runner.then.page_should_contain_text(page, text=iot.omschrijving)
+
+    runner.when.click_on_link(page, name=iot.omschrijving)
+
+    runner.then.path_should_be(
+        page,
+        f"/OZ/{furl(catalogus.url).path.segments[-1]}/informatieobjecttypen/{iot.uuid}",
+    )
+
+    runner.when.click_on_button(page, name="Publiceren")
+
+    runner.then.table_entry_should_contain_aria_label(
+        page, table_header="Concept", attribue_key="aria-label", attribute_value="Nee"
+    )
+
+
+@pytest.mark.e2e
+def test_update_and_publish_informatieobjecttype(page: Page, runner: GherkinRunner):
+    ServiceFactory.create(
+        api_type=APITypes.ztc,
+        api_root="http://localhost:8003/catalogi/api/v1",
+        client_id="test-vcr",
+        secret="test-vcr",
+        slug="OZ",
+    )
+    helper = OpenZaakDataCreationHelper(ztc_service_slug="OZ")
+    catalogus = helper.create_catalogus()
+    assert catalogus.url
+    iot = helper.create_informatieobjecttype(catalogus=catalogus.url)
+    assert iot.url
+
+    UserFactory.create(username="johndoe", password="secret")
+
+    runner.when.go_to_root_page(page)
+    runner.when.user_logs_in(page, username="johndoe", password="secret")
+
+    runner.then.path_should_be(page, "/OZ/")
+
+    runner.when.select_catalogus(page, catalogus)
+    runner.when.go_to_informatieobjecttype_list_page(page, catalogus)
+
+    runner.then.page_should_contain_text(page, text=iot.omschrijving)
+
+    runner.when.click_on_link(page, name=iot.omschrijving)
+
+    runner.then.path_should_be(
+        page,
+        f"/OZ/{furl(catalogus.url).path.segments[-1]}/informatieobjecttypen/{iot.uuid}",
+    )
+
+    runner.when.click_on_button(page, name="Bewerken")
+
+    runner.then.path_should_be(
+        page,
+        f"/OZ/{furl(catalogus.url).path.segments[-1]}/informatieobjecttypen/{iot.uuid}?editing=true",
+    )
+    page.get_by_label("Omschrijving").fill("Updated Omschrijving")
+
+    runner.when.click_on_button(page, name="Opslaan en publiceren")
+
+    runner.then.page_should_contain_text(page, "Updated Omschrijving")
+    runner.then.table_entry_should_contain_aria_label(
+        page, table_header="Concept", attribue_key="aria-label", attribute_value="Nee"
+    )
