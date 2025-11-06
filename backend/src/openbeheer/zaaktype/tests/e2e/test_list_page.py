8c4a28a55af86eece74e68014e3165f5e970eda2
Sven van de Scheur <svenvandescheur@gmail.com>
2025-11-06 17:13:14 +0100

    :white_check_mark: test: update tests for search

--- backend/src/openbeheer/zaaktype/tests/e2e/test_list_page.py
+++ backend/src/openbeheer/zaaktype/tests/e2e/test_list_page.py
@@ -50,3 +50,47 @@ def test_list_zaaktypen(page: Page, runner: GherkinRunner):
     # Navigate to detail view
     _.then.page_should_contain_text(page, "Overzicht")
     _.then.page_should_contain_text(page, zaaktypen[0].omschrijving)
+
+
+@pytest.mark.e2e
+@vcr_overrides(
+    custom_matchers=[
+        ("query_without_datum_geldigheid", matcher_query_without_datum_geldigheid)
+    ],
+    custom_match_on=[
+        "method",
+        "scheme",
+        "host",
+        "port",
+        "path",
+        "query_without_datum_geldigheid",
+    ],
+)
+def test_filter(page: Page, runner: GherkinRunner):
+    _ = runner
+
+    _.given.user_exists()
+    _.given.api_config_exists()
+    _.given.ztc_service_exists()
+    _.given.catalogus_exists()  # Added so that there are 2 catalogi (so the user has to select the one they want)
+
+    catalogus = _.given.catalogus_exists()
+    _.given.zaaktypen_exist(catalogus, 20)
+
+    # Login
+    _.when.user_open_application(page)
+    _.when.user_logs_in(page, username="johndoe", password="secret")
+    _.then.path_should_be(page, "/OZ/")
+
+    # Open list view
+    _.when.user_selects_catalogus(page, catalogus)
+    _.when.user_navigates_to_zaaktype_list_page(page)
+    _.then.page_should_contain_text(page, "ZAAKTYPE-2025-0000000020")
+    _.then.page_should_contain_text(page, "ZAAKTYPE-2025-0000000019")
+    _.then.page_should_contain_text(page, "ZAAKTYPE-2025-0000000018")
+
+    # Search for zaaktype
+    _.when.user_fills_form_field(page, "Zoek op identificatie", "18")
+    _.then.page_should_not_contain_text(page, "ZAAKTYPE-2025-0000000020")
+    _.then.page_should_not_contain_text(page, "ZAAKTYPE-2025-0000000019")
+    _.then.page_should_contain_text(page, "ZAAKTYPE-2025-0000000018")
