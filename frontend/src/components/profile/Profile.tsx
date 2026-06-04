635907cf4cd8bfbf948f2c166d6b10099bad1de4
Sven van de Scheur <svenvandescheur@gmail.com>
2026-06-04 17:19:14 +0200

    :arrow_up: chore: update admin-ui

--- frontend/src/components/profile/Profile.tsx
+++ frontend/src/components/profile/Profile.tsx
@@ -59,7 +59,7 @@ export const Profile: React.FC<ProfileProps> = ({ user }) => {
             <Column span={6} />
             <Column span={6}>
               <NavLink key="logout" to="logout">
-                <Button variant="outline" aria-label="Logout">
+                <Button variant="primary" aria-label="Logout">
                   <Solid.ArrowRightEndOnRectangleIcon /> Uitloggen
                 </Button>
               </NavLink>
