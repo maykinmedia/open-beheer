c72fc4c09f7fbbe137b7d23fddb4e115aca8fa5f
Sven van de Scheur <svenvandescheur@gmail.com>
2026-07-23 17:24:45 +0200

    :white_check_mark: [#385] test: fix the related items e2e test by setting up selectielijst service

--- backend/src/openbeheer/tests/e2e/test_25_edit_related.py
+++ backend/src/openbeheer/tests/e2e/test_25_edit_related.py
@@ -25,6 +25,7 @@ def test_scenario_edit_related_statustypen(page: Page, runner: GherkinRunner):
 
     _.given.user_exists()
     _.given.api_config_exists()
+    _.given.selectielijst_service_exists()
     _.given.ztc_service_exists()
 
     catalogus = _.given.catalogus_exists()
@@ -99,6 +100,7 @@ def test_scenario_edit_related_zaaktypeinformatieobjecttypen(
 
     _.given.user_exists()
     _.given.api_config_exists()
+    _.given.selectielijst_service_exists()
     _.given.ztc_service_exists()
 
     catalogus = _.given.catalogus_exists()
@@ -173,6 +175,7 @@ def test_scenario_edit_related_roltypen(page: Page, runner: GherkinRunner):
 
     _.given.user_exists()
     _.given.api_config_exists()
+    _.given.selectielijst_service_exists()
     _.given.ztc_service_exists()
 
     catalogus = _.given.catalogus_exists()
@@ -260,6 +263,7 @@ def test_scenario_edit_related_resultaattypen(page: Page, runner: GherkinRunner)
 
     _.given.user_exists()
     _.given.api_config_exists()
+    _.given.selectielijst_service_exists()
     _.given.ztc_service_exists()
 
     catalogus = _.given.catalogus_exists()
@@ -347,6 +351,7 @@ def test_scenario_edit_related_eigenschappen(page: Page, runner: GherkinRunner):
 
     _.given.user_exists()
     _.given.api_config_exists()
+    _.given.selectielijst_service_exists()
     _.given.ztc_service_exists()
 
     catalogus = _.given.catalogus_exists()
